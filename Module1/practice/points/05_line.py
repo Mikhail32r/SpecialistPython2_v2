93195bd0fc4381b8fab48caa7a684de62cc9b2fd
Mikhail32r <115464481+Mikhail32r@users.noreply.github.com>
2022-10-10 14:30:28 +0300

    Update 05_line.py

--- Module1/practice/points/05_line.py
+++ Module1/practice/points/05_line.py
@@ -1,19 +1,18 @@
-from __future__ import annotations
-
+import math
 
 class Point:
-    def __init__(self, x: int, y: int):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+def distance(p1:Point, p2:Point) -> float:
+    return math.sqrt((p2.x - p1.x) ** 2 + (p2.y - p1.y) ** 2)
 
-    def dist_to(self, other_point: Point) -> float:
-        # TODO-0: скопируйте реализацию из предыдущей задачи
-        ...
+points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
+idx=1
+leng=0
+while idx< len(points):
+    leng+=distance(points[idx-1],points[idx])
+    idx=idx+1
 
+print("Длина ломаной ", leng)
 
-# Ломаная линия задана произвольным количеством последовательных точек
-points = [Point(2, 4), Point(7, 5), Point(5, -2), Point(0, 6), Point(-12, 0)]
-
-# TODO: Найдите длину ломаной линии
-
-print("Длина ломаной линии = ", ...)
