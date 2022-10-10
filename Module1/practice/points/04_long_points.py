0c7eb70bdcce929e1c2c89abe6ec791d0e9ab348
Mikhail32r <115464481+Mikhail32r@users.noreply.github.com>
2022-10-10 16:27:29 +0300

    Update 04_long_points.py

--- Module1/practice/points/04_long_points.py
+++ Module1/practice/points/04_long_points.py
@@ -1,19 +1,47 @@
-from __future__ import annotations
+class Item:
+    def __init__(self, name: str, weight: float, cost: int):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
 
 
-class Point:
-    def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+class BackPack:  # рюкзак
+    def __init__(self, max_weight: float):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight
+        self.current_wei = 0
+    def add_item(self, item: Item):
+        if self.current_wei+item.weight>self.max_weight:
+            print("Слишком тяжело. Не кладем")
+        else:
+            self.current_wei += item.weight
+            self.items.append(item)
 
-    def dist_to(self, other_point: Point) -> float:
-        # TODO-0: скопируйте реализацию из предыдущей задачи
-        ...
 
 
-# Дан список из произвольного количества точек:
-points = [Point(2, 7), Point(12, 7), Point(5, -2), Point(10, -16), Point(-12, 0)]
+    def show_items(self):
+        print("В рюкзаке лежат", len(self.items), "предметов")
+        allW=0
+        for numobj in range(len(self.items)):
+            print(numobj+1,')',self.items[numobj].name,"стоимостью в", self.items[numobj].cost)
+            allW+=self.items[numobj].weight
+        print("Общий вес (в кг)", allW)
 
-# TODO-1: найдите точку наиболее удаленную от начала координат и выведите ее координаты
 
-print("Координаты наиболее удаленной точки = ", ...)
+# Создаем предметы
+items = [
+Item("Гиря", 25, 500),
+Item("Арбуз", 4, 300),
+Item("Ноутбук", 2.5, 22500),
+Item("Кот", 0.5, 250)
+]
+# Создаем пустой рюкзак
+backpack = BackPack(25)
+
+# Добавляем пару предметов в рюкзак
+for it in items:
+    backpack.add_item(it)
+
+
+# Выводим все предметы в рюкзаке
+backpack.show_items()
