34ab5286fd54148a8f79e81c79cce897df3bcd99
Mikhail32r <115464481+Mikhail32r@users.noreply.github.com>
2022-10-10 22:37:26 +0300

    Update backpack_hw.py

--- Module2/home_works/backpack_hw.py
+++ Module2/home_works/backpack_hw.py
@@ -1,39 +1,98 @@
 class Item:
-    # TODO-0: копируем реализацию всех методов из практики или дописываем самостоятельно
-    def __init__(self, name, weight, cost):
-        ...
+    def __init__(self, name: str, weight: float, cost: int):
+        self.name = name  # Название предмета
+        self.weight = weight  # Вес предмета, в килограммах
+        self.cost = cost  # Цена предмета, пусть будет, в рублях
 
-    def show(self):
-        ...
+    def show(self) -> str:
+        """
+        Возвращает строковое представление объекта Item
+        """
+        return f"{self.name} вес:{self.weight} цена:{self.cost}"
 
 
 class BackPack:  # рюкзак
-    # TODO-0: копируем реализацию всех методов из практики или дописываем самостоятельно
-    def __init__(self, max_weight: float):
-        ...
+    def __init__(self, max_weight):
+        self.items = []  # Предметы, которые хранятся в рюкзаке
+        self.max_weight = max_weight
 
     def add_item(self, item: Item) -> None:
         """
         Добавляет предмет(item) в этот рюкзак
         """
-        ...
+        if self.sum_weight() + item.weight > self.max_weight:
+            print(f"Предмет {item.name} слишком тяжелый")
+        else:
+            self.items.append(item)
 
     def show_items(self) -> None:
         """
-        Вывод все предметы, содержащиеся в рюкзаке в виде нумерованного списка
+        Выводит(print'ом) все предметы, содержащиеся в рюкзаке в виде нумерованного списка
         """
-        ...
+        i = 1
+        for one in self.items:
+            print(i, one.show())
+            i += 1
 
+    def sum_weight(self) -> float:
+        """
+        Возвращает суммарный вес всех предметов в рюкзаке
+        """
+        weight = 0
+        for item in self.items:
+            weight += item.weight
+        return weight
 
-# Создаем рюкзак, указываем максимальный вес
-backpack = BackPack(max_weight=...)
+    def sum_cost(self) -> int:
+        """
+        Возвращает суммарную стоимость всех предметов в рюкзаке
+        """
+        cost = 0
+        for item in self.items:
+            cost += item.cost
+        return cost
 
-# TODO-1: Создаем 5-8 предметов и добавляем их в рюкзак
-...
+    def add_items(self, items: list[Item]):
+        """
+        :param items: Список вещей(объектов класса Item)
+        """
+        items.sort(key=lambda x: x.weight)
+        for item in items:
+            self.add_item(item)
 
-# TODO-2: В рюкзаке найдите самый тяжелый предмет и выведите его на экран
-#  поиск предмета реализуйте в виде метода .max_weight()
+    def max_weight_item(self):
+        """
+        самый тяжелый предмет
+        """
+        i=len(items)-3 #возвращает 5, нумерация с 0 - последний элемент с индексом 4. в конце списка пустота итого минус 3
+        print("Самый тяжелый {} с весом {} кг".format(self.items[i].name,self.items[i].weight ))
 
-# TODO-3: В рюкзаке найдите самый ценный предмет и выведите его на экран
-#  ценным предметом, считать предмет, с самым высоким значение цены на единицу веса
-#  поиск предмета реализуйте в виде метода .max_valuable()
+    def max_valuable(self):
+        """
+        :param items: Список вещей(объектов класса Item)
+        """
+        items.sort(key=lambda x: x.cost/x.weight, reverse=True)
+        print("Самый дорогой {} с соотношением цены к весу {} за кг ".format(items[0].name, items[0].cost/items[0].weight))
+
+# Создаем предметы
+items = [
+     Item("Гиря", 25, 500),
+     Item("Арбуз", 4, 300),
+     Item("Ноутбук", 2.5, 22500),
+     Item("Кот", 0.5, 250),
+     Item("Мяч", 1, 250),
+     Item("Трос", 3, 150),
+]
+
+# Создаем пустой рюкзак и указываем его вместимость(в кг)
+backpack = BackPack(max_weight=7)
+
+# Пытаемся добавлять максимальное кол-во предметов в рюкзак
+backpack.add_items(items)
+
+# Выводим все предметы в рюкзаке
+backpack.show_items()
+
+backpack.max_weight_item()
+
+backpack.max_valuable()
