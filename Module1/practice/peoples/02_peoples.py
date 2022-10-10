747b565c867e82a74a8a8c5796201809cbab9f61
Mikhail32r <115464481+Mikhail32r@users.noreply.github.com>
2022-10-10 15:27:21 +0300

    Update 02_peoples.py

--- Module1/practice/peoples/02_peoples.py
+++ Module1/practice/peoples/02_peoples.py
@@ -5,8 +5,11 @@ class People:
         self.age = age
 
     def change_age(self, new_age: int) -> None:
-        # TODO: скопируйте реализацию метода из предыдущей задачи
-        ...
+        if type(new_age)==int and 1<new_age<100:
+            self.age = new_age
+        else:
+            print("некоррект возраст")
+        return self.age
 
     def full_name(self) -> str:
         return f"{self.surname} {self.name}"
@@ -15,16 +18,31 @@ class People:
         return f"Человек: {self.surname} {self.name} и ему {self.age} лет"
 
 
-# Совет: не забывайте, вы можете добавлять в список и удалять из него любых людей, это просто пример!
+# Создадим двух человек:
 peoples = [
     People("Иван", "Уткин", 27),
     People("Алена", "Перова", 32),
     People("Василий", "Быстров", 55),
     People("Ольга", "Подгорная", 32),
+    People("Ив", "Кин", 27),
+    People("Алексей", "Перов", 32),
+    People("Василий", "Быстров", 65),
 ]
-
-# TODO-1: найдите самого молодого человека и выведите его Фамилию и Имя
-#  Примечание: Если самых молодых несколько, выведите любого
-
-# TODO-2: найдите всех одногодок и выведите их Фамилии и Имена
-#  Примечание: Если одногодок нет, выведите сообщение "одногодок нет"
+min=People("","",101)
+for pep in peoples:
+    if min.age>pep.age:
+        min=pep
+print("Самый молодой",min.full_info())
+odn = []
+while len(peoples)!=0:
+    min=peoples[0]
+    peoples.pop(0)
+    odn.append(min)
+    for pep in peoples:
+        if pep.age==odn[0].age:
+            odn.append(pep)
+            peoples.remove(pep)
+    for od in odn:
+        if len(odn)>1:
+            print(od.full_info())
+    odn.clear()
