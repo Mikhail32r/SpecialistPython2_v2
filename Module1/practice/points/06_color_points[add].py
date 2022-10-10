7a3a4f377e63655abaded53965908d2eb24647d3
Mikhail32r <115464481+Mikhail32r@users.noreply.github.com>
2022-10-10 14:13:58 +0300

    Update 06_color_points[add].py

--- Module1/practice/points/06_color_points[add].py
+++ Module1/practice/points/06_color_points[add].py
@@ -1,33 +1,48 @@
-from __future__ import annotations
-
+import math
 
 class Point:
-    def __init__(self, x: int, y: int, color: str):
+    def __init__(self, x, y,color):
         self.x = x
         self.y = y
-        self.color = ...
-
-    def dist_to(self, other_point: Point) -> float:
-        ...
+        self.color = color
 
 
-# Дан список точек нарисованных красным(red) и зеленым(green) цветами
-# Точно известно что точек каждого цвета ровно три, но порядок точек в списке произвольный
+class Triangle:
+    def __init__(self):
+        self.listpoint = []
+        self.ab = 0
+        self.bc = 0
+        self.ac = 0
+        self.p = 0
+        self.s = 0
+    def add (self, p1):
+        return self.listpoint.append(p1)
+    def calc_s(self):
+         self.ab = math.sqrt((self.listpoint[0].x - self.listpoint[1].x) ** 2 + (self.listpoint[0].y - self.listpoint[1].y) ** 2)
+         self.bc = math.sqrt((self.listpoint[1].x - self.listpoint[2].x) ** 2 + (self.listpoint[1].y - self.listpoint[2].y) ** 2)
+         self.ac = math.sqrt((self.listpoint[0].x - self.listpoint[2].x) ** 2 + (self.listpoint[0].y - self.listpoint[2].y) ** 2)
+         self.p = (self.ab + self.bc + self.ac) / 2
+         self.s = (self.p * (self.p - self.ac) * (self.p - self.bc)*(self.p - self.ab)) ** 0.5
+         return self.s
+
+
 points = [
-    Point(2, 7, "red"),
-    Point(12, 7, "green"),
-    Point(5, -2, "red"),
-    Point(4, 8, "green"),
-    Point(10, -2, "green"),
-    Point(-12, 0, "red")
-]
-# Все точки одного цвета соединены линиями и образуют треугольник
+        Point(2, 7, "red"),
+        Point(12, 7, "green"),
+        Point(5, -2, "red"),
+        Point(4, 8, "green"),
+        Point(10, -2, "green"),
+        Point(-12, 0, "red")
+    ]
+tri_green = Triangle()
+tri_red = Triangle()
 
-# TODO-1: доработайте конструктор class Point для хранения цвета точки
-# TODO-2: реализуйте метод dist_to()
-# TODO-3: вычислите площади треугольников образованных точками разных цветов
 
-# your core here...
+for point in points:
+   if point.color=="green":
+       tri_green.add(point)
+   if point.color == "red":
+       tri_red.add(point)
 
-print("Площадь красного треугольника = ", ...)
-print("Площадь зеленого треугольника = ", ...)
+print("Площадь треугольника(зеленый) = ", tri_green.calc_s())
+print("Площадь треугольника(красный) = ", tri_red.calc_s())
